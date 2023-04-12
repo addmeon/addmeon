284b6c97e7406783b19c6589a33f8515a431225b
Emil Triest <emil@triest.de>
2023-04-12 14:41:20 +0200

    addmeons endpoints

---
+++ src/pages/api/users/addmeons/edit.js
@@ -0,0 +1,44 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import {MongoClient, ServerApiVersion} from "mongodb";
+
+// TODO check username is applicable to route, check user has authenticated email or phone
+// TODO modify addmeons/add new addmeons
+
+export default async function handler(req, res) {
+    console.log(req.body);
+
+    const client = new MongoClient(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1
+    });
+    await client.connect()
+        .catch(err => {
+            client.close();
+            console.error(err);
+        });
+    const collection = client
+        .db(process.env.MONGODB_DATABASE_NAME)
+        .collection("users");
+
+    const user = await collection.findOne({
+        email: req.body.email
+    });
+
+    if (!user) {
+        await client.close();
+        return res.status(400).json({error: "user doesnt exist"});
+    }
+
+    try {
+        // TODO: validation of req.body
+        const addMeOnsCopy = user.addMeOns;
+        addMeOnsCopy[req.body.key] = {link: req.body.value}
+        await collection.updateOne({email: req.body.email}, {$set: {addMeOns: addMeOnsCopy}});
+        await client.close();
+        return res.status(200).json({addMeOns: addMeOnsCopy});
+    } catch (err) {
+        return res.status(503).json({error: err});
+    }
+}
