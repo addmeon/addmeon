155a805e353435812ed15ee2affe6fed7f97d542
Emil Triest <emil@triest.de>
2023-07-29 16:43:19 +0800

    disable input zoom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,31 @@
-import { AppProps } from 'next/app';
+import {AppProps} from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import {MantineProvider} from '@mantine/core';
 import {useViewportSize} from "@mantine/hooks";
 
 export default function App(props) {
-  const { Component, pageProps } = props;
+    const {Component, pageProps} = props;
 
-    const { height, width } = useViewportSize();
-    const isMobile=height>width;
+    const {height, width} = useViewportSize();
+    const isMobile = height > width;
 
-  return (
-      <>
-        <Head>
-          <title>Page title</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        </Head>
+    return (
+        <>
+            <Head>
+                <title>Page title</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+            </Head>
 
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{
-              /** Put your mantine theme override here */
-              colorScheme: 'light',
-            }}
-        >
-          <Component {...pageProps} isMobile={isMobile} userAgentHeight={height} userAgentWidth={width} />
-        </MantineProvider>
-      </>
-  );
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    /** Put your mantine theme override here */
+                    colorScheme: 'light',
+                }}
+            >
+                <Component {...pageProps} isMobile={isMobile} userAgentHeight={height} userAgentWidth={width}/>
+            </MantineProvider>
+        </>
+    );
 }
